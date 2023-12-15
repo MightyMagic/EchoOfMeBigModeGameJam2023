7002a7cfeb23273734dceb7c045f224c59f29dfe
Agustin Anfuso <anfusoagustin89@gmail.com>
2023-12-15 14:28:32 +0400

    Add furniture list to terrain

--- EchoOfMe/Assets/Scenes/RoomGeneration/RoomGenerator.cs
+++ EchoOfMe/Assets/Scenes/RoomGeneration/RoomGenerator.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class RoomGenerator : MonoBehaviour
 {
+    [Header("Room Properties")]
+    public GameObject[] forniture;
+
     // Start is called before the first frame update
     void Start()
     {

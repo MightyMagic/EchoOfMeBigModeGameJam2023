3185a2e68498a9dc6ae60004aa3846078edd78d8
Agustin Anfuso <anfusoagustin89@gmail.com>
2023-12-15 14:37:50 +0400

    Update furniture list

--- EchoOfMe/Assets/Scenes/RoomGeneration/RoomGenerator.cs
+++ EchoOfMe/Assets/Scenes/RoomGeneration/RoomGenerator.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class RoomGenerator : MonoBehaviour
 {
     [Header("Room Properties")]
-    public GameObject[] forniture;
+    public GameObject[] furnitureSelection;
 
     // Start is called before the first frame update
     void Start()
